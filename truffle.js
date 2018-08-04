ebd38fda12a2cd398c5dfb5379403b4914a3dfd4
Antony Kimpton <antony.kimpton@kcl.ac.uk>
2018-08-04 17:24:06 +0100

    refactor truffle config

--- truffle.js
+++ truffle.js
@@ -13,12 +13,12 @@
  */
 
 
-  module.exports = {
-    networks: {
-      development: {
-        host: "127.0.0.1",
-        port: 7545,
-        network_id: "*" // Match any network id
-      }
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*' // Match any network id
     }
-  };
+  }
+};
